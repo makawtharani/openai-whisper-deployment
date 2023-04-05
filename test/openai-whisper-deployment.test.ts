61834e67036f408036d42de95640353d8ccb5bad
Mohamad Albaker Kawtharani <kawtharanibaqer@gmail.com>
2023-04-05 10:42:46 +0300

    Initial commit

---
+++ test/openai-whisper-deployment.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OpenaiWhisperDeployment from '../lib/openai-whisper-deployment-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/openai-whisper-deployment-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OpenaiWhisperDeployment.OpenaiWhisperDeploymentStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
