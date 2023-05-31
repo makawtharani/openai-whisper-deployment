4d9cb4c3a49d6ae79e1ea13f40be00912a186aba
Mohamad Albaker Kawtharani <kawtharanibaqer@gmail.com>
2023-05-31 15:49:19 +0300

    update handler

---
+++ whisper-invoke/whisper.py
@@ -0,0 +1,51 @@
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+import json
+
+# Initialize the Boto3 clients
+sagemaker_runtime = boto3.client('sagemaker-runtime')
+s3 = boto3.client('s3')
+
+# Endpoint and SNS Topic names
+endpoint_name = '' # enter the name
+
+s3_bucket = '' # enter s3 bucket for the file you want to transcribe
+s3_key = '' # enter s3_key for the file you want to transcribe
+job_id= '' # enter job id
+language= '' # enter language
+
+payload = {
+    "s3_bucket":s3_bucket,
+    "s3_key": s3_key,
+    "job_id": job_id,
+    "language": language
+}
+
+s3_key_for_input = s3_key.rsplit('/', 1)[0] + '/asynch_input_file.json'
+
+# Save payload to S3
+try:
+    s3.put_object(
+        Body=json.dumps(payload),
+        Bucket=s3_bucket,
+        Key=s3_key_for_input
+    )
+    print("File Saved.")
+except BotoCoreError as e:
+    print(f"Error saving JSON to S3: {e}")
+
+# The S3 location of the input data for the inference request
+input_location = f"s3://{s3_bucket}/{s3_key_for_input}"
+print(input_location)
+
+# Asynchronous Inference
+try:
+    response = sagemaker_runtime.invoke_endpoint_async(
+        EndpointName=endpoint_name,
+        InputLocation=input_location,
+        ContentType='application/json',
+        InferenceId='inference-test-id',
+    )
+    print(f"response: {response}")
+except BotoCoreError as e:
+    print(f"Error invoking endpoint: {e}")
--- whisper-sns-lambda/handler.py
+++ whisper-sns-lambda/handler.py
@@ -6,14 +6,20 @@ dynamodb = boto3.resource('dynamodb')
 table = dynamodb.Table(os.getenv('TABLE_NAME'))
 
 def main(event, context):
+    print(f"event {event}")
     for record in event['Records']:
         message = json.loads(record['Sns']['Message'])
-        jobId = message['jobId']
-        result = message['result']
+        print(f"message {message}")
+        
+        jobId = message['inferenceId']
+        result = message['invocationStatus']
+        eventTime = message['eventTime']
+        
         table.put_item(
             Item={
                 'jobId': jobId,
-                'result': result
+                'result': result,
+                'eventTime': 
             }
         )
     return {
